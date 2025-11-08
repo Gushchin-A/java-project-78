b6fd9352418cfd2447730722eda98cf9e723230e
Andrei Gushchin <kpa6ep@gmail.com>
2025-11-08 17:54:08 +0100

    project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("java")
+    id("org.sonarqube") version "6.2.0.5505"
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Gushchin-A_java-project-71")
+        property("sonar.organization", "gushchin-a")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property("sonar.coverage.jacoco.xmlReportPaths",
+            "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
